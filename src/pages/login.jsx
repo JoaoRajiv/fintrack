b12ea25268633a9f51fc123c72d30729b37b2523
JoaoRajiv <olivairarajiv@gmail.com>
2026-02-16 21:12:18 -0300

    feat: add api integration to login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -17,9 +17,13 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import api from '@/lib/axios';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { toast } from 'sonner';
 import z from 'zod';
 
 const signupSchema = z.object({
@@ -28,13 +32,12 @@ const signupSchema = z.object({
     .email({ message: 'Email inválido' })
     .trim()
     .min(1, { message: 'O email é obrigatório' }),
-  password: z
-    .string()
-    .trim()
-    .min(6, { message: 'A senha deve conter pelo menos 6 caracteres' }),
+  password: z.string(),
 });
 
 const LoginPage = () => {
+  const [user, setUser] = useState(null);
+
   const form = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -42,9 +45,77 @@ const LoginPage = () => {
       password: '',
     },
   });
+
+  const loginMutation = useMutation({
+    mutationKey: ['login'],
+    mutationFn: async (variables) => {
+      const response = await api.post('/users/login', {
+        email: variables.email,
+        password: variables.password,
+      });
+      return response.data;
+    },
+  });
+
   const handleSubmit = (data) => {
-    console.log('Dados do formulário:', data);
+    loginMutation.mutate(data, {
+      onSuccess: (loggedUser) => {
+        const accessToken = loggedUser.tokens.accessToken;
+        const refreshToken = loggedUser.tokens.refreshToken;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        setUser(loggedUser);
+        toast.success('Login feito com sucesso.');
+      },
+      onError: (error) => {
+        toast.error('Email ou senha inválidos, tente novamente.');
+        console.log('Erro ao entrar na conta:', error);
+      },
+    });
   };
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    setUser(null);
+    toast.success('Logout feito com sucesso.');
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken');
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (!accessToken && !refreshToken) return;
+        const response = await api.get('/users/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.log('Erro ao obter usuário:', error);
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+      }
+    };
+    init();
+  }, []);
+
+  if (user) {
+    return (
+      <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+        <h1 className="text-2xl font-bold">Bem-vindo, {user.first_name}!</h1>
+        <p className="text-muted-foreground">
+          Você entrou na sua conta com sucesso. Agora você pode acessar o
+          dashboard.
+        </p>
+        <Button variant="outline" onClick={handleLogout}>
+          Sair
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
