aa71b86ee1aeabddeab078b84731edc6abf48f12
JoaoRajiv <olivairarajiv@gmail.com>
2026-02-19 17:30:19 -0300

    feat: add JSdoc on user methods

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,15 @@
 import { api } from '@/lib/axios';
 
 export const UserService = {
+  /**
+   * Cria um novo usuário com os dados fornecidos.
+   * @param {Object} input - Dados do usuário.
+   * @param {string} input.firstName - Primeiro nome do usuário.
+   * @param {string} input.lastName - Sobrenome do usuário.
+   * @param {string} input.email - Email do usuário.
+   * @param {string} input.password - Senha do usuário.
+   * @returns {Object} - Dados do usuário criado, incluindo tokens de autenticação.
+   */
   signUp: async (input) => {
     const response = await api.post('/users', {
       first_name: input.firstName,
@@ -16,6 +25,13 @@ export const UserService = {
       tokens: response.data.tokens,
     };
   },
+  /**
+   * Faz login de um usuário com os dados fornecidos.
+   * @param {Object} input - Dados do usuário para login.
+   * @param {string} input.email - Email do usuário.
+   * @param {string} input.password - Senha do usuário.
+   * @returns {Object} - Dados do usuário logado, incluindo tokens de autenticação.
+   */
   login: async (input) => {
     const response = await api.post('/users/login', {
       email: input.email,
@@ -29,6 +45,10 @@ export const UserService = {
       tokens: response.data.tokens,
     };
   },
+  /**
+   * Obtém os dados do usuário atualmente autenticado.
+   * @returns {Object} - Dados do usuário autenticado.
+   */
   me: async () => {
     const response = await api.get('/users/me');
     return {
